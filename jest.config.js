94c6dabb1de125420aeee64fcb03feb1434413d6
quang-kiet.tran <quang-kiet.tran@ekino.com>
2019-02-21 19:09:38 +0700

    fix issues when init jest test

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  transform: { '^.+\\.tsx?$': 'ts-jest' },
-  testEnvironment: 'jsdom',
-  // testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-}
