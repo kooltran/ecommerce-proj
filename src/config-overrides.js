575430400d71a00fd08470f861d02e9d7ac52e8f
Tue Vu <tuevu0171@gmail.com>
2019-02-24 12:27:37 +0700

    [fix] add package type

--- src/config-overrides.js
+++ src/config-overrides.js
@@ -1,24 +1,24 @@
-const {
+import {
   override,
   addDecoratorsLegacy,
   disableEsLint,
   addBundleVisualizer,
   addWebpackAlias,
-  adjustWorkbox
-} = require("customize-cra");
-const path = require("path");
+  adjustWorkbox,
+} from 'customize-cra'
+import { resolve } from 'path'
 
 module.exports = override(
   addDecoratorsLegacy(),
   disableEsLint(),
   process.env.BUNDLE_VISUALIZE == 1 && addBundleVisualizer(),
   addWebpackAlias({
-    ["ag-grid-react$"]: path.resolve(__dirname, "src/shared/agGridWrapper.js")
+    'ag-grid-react$': resolve(__dirname, 'src/shared/agGridWrapper.js'),
   }),
   adjustWorkbox(wb =>
     Object.assign(wb, {
       skipWaiting: true,
-      exclude: (wb.exclude || []).concat("index.html")
+      exclude: (wb.exclude || []).concat('index.html'),
     })
   )
-);
+)
