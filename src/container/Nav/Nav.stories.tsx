fd766dec06a1fb76bd0ace2b9c4aed3d3e606c14
Anh Dai <lanhdai1988@gmail.com>
2019-03-31 10:30:55 +0700

    feat(COMMON-NAV): common nav

--- src/container/Nav/Nav.stories.tsx
+++ src/container/Nav/Nav.stories.tsx
@@ -2,13 +2,12 @@ import React from 'react'
 
 import { storiesOf } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
-import dataList from './data'
 import Nav from './Nav'
 
-storiesOf('Ecommerce/Common/Nav', module)
+storiesOf('Ecommerce/UI-Kit/Nav', module)
   .add(
     'Default',
     withInfo({ header: true })(() => (
-      <Nav data={dataList}/>
+      <Nav />
     ))
   )
