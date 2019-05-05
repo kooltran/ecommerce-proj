c2b68e2fc6fffc18c65d46f895d50f728718fbeb
Anh Dai <lanhdai1988@gmail.com>
2019-05-05 16:38:10 +0700

    fix(COMMON-NAV): update left nav

--- src/container/Nav/data.ts
+++ src/container/Nav/data.ts
@@ -1,11 +1,31 @@
 const data = [
   {
     id: '1',
-    name: 'nam'
+    name: 'nam',
+    subData: [
+      {
+        id: '1',
+        name: 'nam 01',
+      },
+      {
+        id: '2',
+        name: 'nam 02',
+      },
+    ]
   },
   {
     id: '2',
-    name: 'nu'
+    name: 'nu',
+    subData: [
+      {
+        id: '1',
+        name: 'nu 01',
+      },
+      {
+        id: '2',
+        name: 'nu 02',
+      },
+    ]
   }
 ]
 
